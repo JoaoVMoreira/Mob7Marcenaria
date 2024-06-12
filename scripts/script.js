c12be3e80fb39878f0724a05a0ca7a64385c91b4
JoaoVMoreira <jvittormoreira@outlook.pt>
2024-06-11 21:33:35 -0300

    responsividade menu e header

--- scripts/script.js
+++ scripts/script.js
@@ -20,3 +20,16 @@ function prevSlide() {
 
 // Inicializar o carrossel
 showSlide(currentIndex);
+
+
+
+// script.js
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+});
+
